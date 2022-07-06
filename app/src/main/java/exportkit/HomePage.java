27ce9ec151d8d96f34ae06381924a877685dfb66
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-07-06 17:50:53 +0200

    init

---
+++ app/src/main/java/exportkit/HomePage.java
@@ -0,0 +1,15 @@
+package exportkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import exportkit.figma.R;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+    }
+}
