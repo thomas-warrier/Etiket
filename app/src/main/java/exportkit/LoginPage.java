ea3a4d0bf0b2ec88311e9631dc068d4adf4c8609
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-07-08 13:39:55 +0200

    avancement de la page de login

---
+++ app/src/main/java/exportkit/LoginPage.java
@@ -0,0 +1,15 @@
+package exportkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import exportkit.figma.R;
+
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
