d8f0fbe2c712b8699b83c390147dfaff8fd27bbb
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-07-10 00:32:58 +0200

    création de la page d'inscription et correction de quasiment tout les warnings et erreurs

--- app/src/main/java/exportkit/RegisterPage.java
+++ app/src/main/java/exportkit/RegisterPage.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import exportkit.figma.R;
 
-
-public class LoginPage extends AppCompatActivity {
+public class RegisterPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
-
+        setContentView(R.layout.activity_register_page);
     }
 }
