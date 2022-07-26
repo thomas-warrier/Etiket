6a3990f2da17a2d8d12b5df8d569af0efd2d3fda
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-07-26 14:35:48 +0200

    ajout de la classe Ticket et avancement de la listView

---
+++ app/src/main/java/exportkit/Ticket.java
@@ -0,0 +1,78 @@
+package exportkit;
+
+import android.media.Image;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.sql.Date;
+
+
+/**
+ *
+ * @author twarr
+ */
+public class Ticket {
+    private Date date;
+    private String titre;
+    private String description;
+    private String image;
+    private boolean favoris;
+    private String id;
+
+    public Ticket(Date date, String titre, String description, String image, String id) {
+        this.date = date;
+        this.titre = titre;
+        this.description = description;
+        this.image = image;
+        this.favoris = false;
+        this.id = id;
+
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Image getImage() {
+        /*faire un systéme qui va chercher l'image qui a l'id*/
+        return null;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isFavoris() {
+        return favoris;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setFavoris(boolean favoris) {
+        this.favoris = favoris;
+    }
+
+    public boolean isRecent(Ticket ticket) {
+        LocalDateTime localTime = LocalDateTime.now();
+        long diff = ChronoUnit.DAYS.between(localTime, (Temporal) ticket.date);
+        if (diff <= 3) {
+            return true;
+        }
+        return false;
+    }
+}
